b0e4dfbb3160da32544b34cc7e636e7edec9f893
Neo-Ken-Dev <neo-ken-dev@gmail.com>
2021-02-15 15:04:29 +0100

    mise en place de security + fixture participants

--- src/Entity/Etats.php
+++ src/Entity/Etats.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\entity;
+namespace App\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 
@@ -15,9 +15,9 @@ class Etats
     /**
      * @var int
      *
-     * @ORM\Column(name="no_etat", type="integer", nullable=false)
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="IDENTITY")
+     * @ORM\Column(name="no_etat", type="integer", nullable=false)
      */
     private $noEtat;
 
