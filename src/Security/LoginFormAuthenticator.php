91ce0304fd3b92a0d5f8a992894cc1b40032ab14
Neo-Ken-Dev <neo-ken-dev@gmail.com>
2021-02-17 19:00:45 +0100

    change link after connexion to sorties_list

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -106,7 +106,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
             return new RedirectResponse($targetPath);
         }
 
-        return new RedirectResponse($this->urlGenerator->generate('home'));
+        return new RedirectResponse($this->urlGenerator->generate('sorties_list'));
     }
 
     protected function getLoginUrl()
